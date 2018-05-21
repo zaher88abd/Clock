9858688a78ac79c3ad4d1a54f3d495aeda90cfc6
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-05-21 18:07:30 -0300

    Modify the script to make it move

--- Clock/Assets/Clock.cs
+++ Clock/Assets/Clock.cs
@@ -1,22 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Clock : MonoBehaviour {
-
+public class Clock : MonoBehaviour
+{
+    const float degreesPerHour = 30f;
+    const float degreesPerMinute = 6f;
+    const float degreesPerSecond = 6f;
     public Transform hoursTransform, minutesTransform, secondsTransform;
     private void Awake()
     {
-        
+        hoursTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Second * degreesPerSecond, 0f);
     }
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
