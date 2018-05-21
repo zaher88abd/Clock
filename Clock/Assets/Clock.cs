ec2a9b1acef204300c936f46180923444bec4684
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-05-21 18:26:29 -0300

    Done
    Add continuously update

--- Clock/Assets/Clock.cs
+++ Clock/Assets/Clock.cs
@@ -9,22 +9,43 @@ public class Clock : MonoBehaviour
     const float degreesPerMinute = 6f;
     const float degreesPerSecond = 6f;
     public Transform hoursTransform, minutesTransform, secondsTransform;
+    public bool continuous;
+
+
     private void Awake()
     {
-        hoursTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
-        minutesTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Minute * degreesPerMinute, 0f);
-        secondsTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Second * degreesPerSecond, 0f);
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
     }
-
     // Use this for initialization
     void Start()
     {
 
     }
 
+    private void UpdateContinuous()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
+    }
+
+    private void UpdateDiscrete()
+    {
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+    }
+
     // Update is called once per frame
     void Update()
     {
-
+        if (continuous)
+            UpdateContinuous();
+        else UpdateDiscrete();
     }
 }
